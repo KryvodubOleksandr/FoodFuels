59c7811702b2f46f9919be9312f0e8bb4ada8319
Alexander Sharko <krivodubag@gmail.com>
2021-06-11 14:50:02 +0300

    Initial Commit

---
+++ FoodFuels/FoodFuelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodFuelsApp.swift
+//  FoodFuels
+//
+//  Created by Alexander Sharko on 11.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct FoodFuelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
